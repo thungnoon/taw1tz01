180cf5c3d658eb6e83e963d002077123ccd177d4
thungnoon <kongzheng.zhou@gmail.com>
2025-05-25 06:04:02 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -3,6 +3,7 @@
 #更改默认地址为192.168.5.1
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-
+# 给config下的文件增加权限
+chmod 644 files/etc/config/*
 
 echo "init-settings executed successfully!"
