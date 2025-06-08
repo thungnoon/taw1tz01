2d6fcb35a0649f504836f510f74d5f3e17cfc68e
thungnoon <kongzheng.zhou@gmail.com>
2025-06-08 09:53:27 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -53,7 +53,7 @@ git clone -b main --depth 1 https://github.com/linkease/istore-ui.git
 git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
 
 # #fchomo 
-git clone -b master --depth 1 https://github.com/fcshark-org/openwrt-fchomo.git
+# git clone -b master --depth 1 https://github.com/fcshark-org/openwrt-fchomo.git
 
 popd
 
