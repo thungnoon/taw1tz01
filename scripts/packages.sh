070b72858607953543a871a2ca2ba2eb3bef8e26
thungnoon <kongzheng.zhou@gmail.com>
2025-07-18 12:51:03 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -33,7 +33,8 @@ git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 
 # #mosdns
-git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+# git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+# git clone -b master --depth 1 https://github.com/sbwml/v2ray-geodata.git
 
 # #openclash
 # git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
