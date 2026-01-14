182daf1254f49c02770314f16c5044bde5703b00
thungnoon <kongzheng.zhou@gmail.com>
2026-01-14 20:03:13 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 #删除feeds中的插件
-# rm -rf ./feeds/packages/net/v2ray-geodata
-rm -rf ./feeds/packages/lang/golang
-rm -rf ./feeds/packages/net/{geoview,shadowsocks-libev,chinadns-ng,mosdns}
-rm -rf ./feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/lang/golang
+rm -rf ./feeds/packages/net/{geoview,chinadns-ng,hysteria,mosdns,v2ray-geodata}
+rm -rf ./feeds/packages/net/{shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev}
+rm -rf ./feeds/packages/net/{sing-box,v2ray-geodata,v2ray-plugin,xray-core}
+rm -rf ./feeds/luci/applications/{luci-app-mosdns,luci-app-passwall}
 
 #克隆依赖插件
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
 
 #克隆的源码放在small文件夹
 mkdir package/small
 pushd package/small
+
+# luci-theme-aurora
+git clone -b master --depth 1 https://github.com/eamonxg/luci-theme-aurora.git
+
+# luci-app-nft-timecontrol
+git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-timecontrol.git
 
 #adguardhome
 git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
@@ -28,16 +35,19 @@ git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
 # #ssrp
 # git clone -b master --depth 1 https://github.com/fw876/helloworld.git
 
-# #passwall
-# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+## passwall
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 
-#passwall2
+# passwall2
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 
-# #mosdns
+# mosdns
 git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 
-# #openclash
+# luci-app-netspeedtest
+git clone -b master --depth 1 https://github.com/sirpdboy/luci-app-netspeedtest.git
+
+# openclash
 # git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
 
 # #poweroffdevice
@@ -50,11 +60,17 @@ git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-partexp.git
 git clone -b main --depth 1 https://github.com/linkease/istore.git
 git clone -b main --depth 1 https://github.com/linkease/istore-ui.git
 
-# #nikki 
+# OpenWrt-nikki
 git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
 
 # #fchomo 
 # git clone -b master --depth 1 https://github.com/fcshark-org/openwrt-fchomo.git
+
+# OpenWrt-momo
+git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-momo.git
+
+# daed
+git clone -b master --depth 1 https://github.com/QiuSimons/luci-app-daed.git
 
 popd
 
